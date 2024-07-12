f325eb8a3059128190f7803d281a7cec1b4e7290
Brice Nelson <bnelsonemail@icloud.com>
2024-07-11 21:26:37 -0400

    revisions to JS.  Console.log provides desired results.  Errors with event listeners

--- swApp.js
+++ swApp.js
@@ -8,9 +8,8 @@ async function getSWPlanet() {
     ul.append(nameHeading, SWContain);
     try {
         const response = await axios.get('https://swapi.dev/api/planets');
-        console.log(`response: ${response.data.results}`)
         for (let planet of response.data.results) {
-            console.log(`Planets of Star Wars: ${planet.name}`);
+            console.log(`Planets of Star Wars: ${planet.name} climate: ${planet.climate}`);
             ul.append(makePlanetLI(planet));
         }
     } catch (error) {
@@ -27,24 +26,33 @@ function makePlanetLI(planet){
     return newLI;
 }
 
-// TODO: the below function has not been revised
-async function getSWPlanet() {
-    const ul = document.getElementById('planets');
-    const SWContain = document.getElementById('planetNames');
-    const nameHeading = document.createElement('h2');
-    nameHeading.innerText = 'Planet Names';
-    ul.innerText = '';
-    ul.append(nameHeading, SWContain);
+// Planet Data: Climate
+async function getSWPlanetData() {
+    const ulData = document.getElementById('planetClimateList');
+    const SWDataContain = document.getElementById('starWarsDataContainer');
+    const dataHeading = document.createElement('h2');
+    dataHeading.innerText = 'Planet Names';
+    ulData.innerText = '';
+    ulData.append(dataHeading, SWDataContain);
     try {
         const response = await axios.get('https://swapi.dev/api/planets');
         console.log(`response: ${response.data.results}`)
         for (let planet of response.data.results) {
-            console.log(`Planets of Star Wars: ${planet.name}`);
-            ul.append(makePlanetLI(planet));
+            console.log(`Climate of Planets of Star Wars: ${planet.name} : ${planet.climate}`);
+            ulData.append(makePlanetDataLI(planet));
         }
     } catch (error) {
         console.error('Error fetching data:', error);
     }
+}
+
+
+function makeDataPlanetLI(planet){
+    const newLI = document.createElement('LI');
+    const planetName = document.createElement('B');
+    planetName.innerText = planet.name + ': ' + planet.climate;
+    newLI.append(planetName);
+    return newLI;
 }
 
 
@@ -56,11 +64,13 @@ planetBtn.addEventListener('click', () => {
         const planetDataBtn = document.createElement('button')
         planetDataBtn.id = 'planetData';
         planetBtn.parentNode.replaceChild(planetDataBtn, planetBtn); 
-        planetDataBtn.innerText = 'Get Planet Data';
+        planetDataBtn.innerText = 'Get Planet Climate';
         planetDataContainer.append(planetDataBtn);
-    }, 2500)
+        planetDataBtn.addEventListener('click', () => {
+            getSWPlanetData()
+        }); // Move the event listener here
+        
+    }, 2000)
 });
-
-
 
 
